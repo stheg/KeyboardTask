d2664832506b6597b897a354c6f175d79385b4cf
Sergei Pavlov <gsergius@mail.ru>
2022-05-23 11:35:54 +0300

    Add project files.

---
+++ Assets/MyInputField.cs
@@ -0,0 +1,14 @@
+
+using System;
+using UnityEngine;
+
+public class MyInputField : InputFieldOriginal
+{
+    /// <summary>
+    /// Depending on the current selection state, the character will be added either instead of the selected text or to the current caret position
+    /// </summary>
+    /// <param name="c">Character to add</param>
+    internal void AddCharacter(string c)
+    {
+    }
+}
---
+++ Assets/VirtualKeyboard.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VirtualKeyboard : MonoBehaviour {
+
+    public MyInputField InputField;
+
+    public void KeyPress(string c)
+    {
+        InputField.AddCharacter(c);
+        InputField.ActivateInputField();
+    }
+
+    public void KeyLeft()
+    {
+
+    }
+
+    public void KeyRight()
+    {
+
+    }
+
+    public void KeyDelete()
+    {
+
+    }
+}
