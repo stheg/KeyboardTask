161d3bdd4aece6ff745e7caf5a8cdcc9b7fd4298
Sergei Pavlov <gsergius@mail.ru>
2022-05-24 11:20:00 +0300

    multiple keystrokes on the button hold

--- Assets/VirtualKeyboard.cs
+++ Assets/VirtualKeyboard.cs
@@ -1,31 +1,85 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class VirtualKeyboard : MonoBehaviour {
 
     [SerializeField]
     private MyInputField _inputField;
 
-    public void KeyPress(string c)
+    [SerializeField]
+    float _pressedActionStartDelay = 0.5f;
+    [SerializeField]
+    AnimationCurve _pressedActionDelay;
+
+    Event _pressed;
+    Coroutine _holdCoroutine = null;
+
+    #region Keyboard (Button.OnClick)
+
+    public void CharacterKeyPressed(string c) => HandleKeyboardEvent(WrapIntoEvent(c));
+
+    public void KeyLeft() => HandleKeyboardEvent(WrapIntoEvent(KeyCode.LeftArrow));
+
+    public void KeyRight() => HandleKeyboardEvent(WrapIntoEvent(KeyCode.RightArrow));
+
+    public void KeyDelete() => HandleKeyboardEvent(WrapIntoEvent(KeyCode.Backspace));
+
+    #endregion
+
+    #region Holding a key (Button.EventTrigger)
+
+    public void VirtualKeyDown(string input)
     {
-        _inputField.ProcessEvent(new Event() { character = c[0] });
+        _pressed = WrapIntoEvent(input);
+        _holdCoroutine = StartCoroutine(PressKey());
+    }
+
+    public void VirtualKeyUp(string input)
+    {
+        _pressed = null;
+        StopCoroutine(_holdCoroutine);
+    }
+    
+    #endregion
+
+    private void HandleKeyboardEvent(Event e)
+    {
+        _inputField.ProcessEvent(e);
         _inputField.Refresh();
     }
 
-    public void KeyLeft() => KeyCodePressed(KeyCode.LeftArrow);
-
-    public void KeyRight() => KeyCodePressed(KeyCode.RightArrow);
-
-    public void KeyDelete() => KeyCodePressed(KeyCode.Backspace);
-
-    private void KeyCodePressed(KeyCode keyCode)
+    private Event WrapIntoEvent(string input)
     {
-        _inputField.ProcessEvent(new Event() { keyCode = keyCode });
-        _inputField.Refresh();
-    }
+        Event e = new Event();
+        if (input?.Length == 1)
+            e.character = input[0];
+        if (System.Enum.TryParse(input, out KeyCode keyCode))
+            e.keyCode = keyCode;
 
+        //if (_isShiftPressed)
+        //    e.modifiers |= EventModifiers.Shift;
+
+        return e;
+    }
+    private Event WrapIntoEvent(KeyCode keyCode) => WrapIntoEvent(keyCode.ToString());//new Event() { keyCode = keyCode };
+    
     private void Start()
     {
         if (!_inputField.isFocused)
             _inputField.ActivateInputField();
+    }
+
+    IEnumerator PressKey()
+    {
+        yield return new WaitForSecondsRealtime(_pressedActionStartDelay);
+
+        float startTime = Time.time;
+        while (_pressed != null)
+        {
+            HandleKeyboardEvent(_pressed);
+
+            yield return new WaitForSecondsRealtime(_pressedActionDelay.Evaluate(Time.time - startTime));
+        }
+        _holdCoroutine = null;
     }
 }
